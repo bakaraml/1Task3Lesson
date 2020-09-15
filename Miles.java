eb727b909d6071df1f449db09908f57771210e6c
bakaraml <liloek2002@gmail.com>
2020-09-15 08:48:13 +0300

    add Miles.java

---
+++ Miles.java
@@ -0,0 +1,8 @@
+public class Miles {
+    public static void main (String[] args) {
+        long balance = 10;
+        long voice = 49857600;
+        long miles =  voice / 100 / 20  + balance;
+        System.out.println(miles);
+    }
+}
